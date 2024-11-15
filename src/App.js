cd868306297f500334bb3a1ccf6d7fdc396e7506
Marco Bossle Villanova <marco.bossle.villanova@gmail.com>
2024-11-14 21:22:44 -0300

    Added bootstrap, bugfixes and updated projects section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './Navigation/Navbar';
 import About from './Sections/About'
--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -1,65 +1,57 @@
 import './Projects.css'
 
-import projImg1 from "../Images/project1.jpg";
-import projImg2 from "../Images/project2.jpg";
-import projImg3 from "../Images/project3.jpg";
+import Carousel from 'react-bootstrap/Carousel';
+
+import exampleImg from "../Images/exampleImg.jpg";
+import dinosBanner from "../Images/dinosBanner.png"
 
 function Projects()
 {
-    /*
-    var baseImgClass = 'FeaturedImg';
-    var activeImgClass = ' ActiveImg';
-    */
-
-    var baseFeatureClass = 'carroussel-project'
-    var activeFeatureClass = ' carroussel-active'
-
-    var activeFeaturedIndex = 0;
-
-    function moveCarroussel(dir)
-    {
-        let elements = document.getElementsByClassName(baseFeatureClass);
-
-        if (elements.length <= 0) return;
-
-        activeFeaturedIndex += dir;
-        if (activeFeaturedIndex >= elements.length) activeFeaturedIndex = 0;
-        else if (activeFeaturedIndex < 0) activeFeaturedIndex = elements.length - 1;
-
-        for (let i = 0; i < elements.length; i++)
-        {
-            elements[i].classList = baseFeatureClass;
-        }
-
-        elements[activeFeaturedIndex].classList = baseFeatureClass + activeFeatureClass;
-    }
-
     return (
     <>
-        <div id='ProjectsSection' className='ProjectsSection'>
-            <div className='FeaturedProjects'>
-                <div className={baseFeatureClass + activeFeatureClass} style={{backgroundImage: `url(${projImg1})`}}>
-                    <div className='carroussel-content'>
-                        <button>SEE MORE</button>
-                        <p>OHHH MAH GAH</p>
-                    </div>
-                </div>
-                <div className={baseFeatureClass} style={{backgroundImage: `url(${projImg2})`}}>
-                <div className='carroussel-content'>
-                        <button>SEE MORE</button>
-                        <p>OHHH MAH GAH 2</p>
-                    </div>
-                </div>
-                <div className={baseFeatureClass} style={{backgroundImage: `url(${projImg3})`}}>
-                <div className='carroussel-content'>
-                        <button>SEE MORE</button>
-                        <p>OHHH MAH GAH 3</p>
-                    </div>
-                </div>
-                <button className='carroussel-prev' onClick={(e) => moveCarroussel(-1, e)}>❮</button>
-                <button className='carroussel-next' onClick={(e) => moveCarroussel(1, e)}>❯</button>
-            </div>
+    <div id='ProjectsSection' className='projects'>
+        {/* CAROUSEL DE PROJETOS IMPORTANTES /*/}
+        <div className='featured-projects'>
+            <Carousel>
+                <Carousel.Item interval={null}>
+                    <img
+                        className="d-block w-100" src={dinosBanner}
+                        alt="Dinos banner"
+                    />
+                    <Carousel.Caption>
+                        <button>Learn more</button>
+                        <p>Dinos is a multiplayer clone of the famous offline browser game: chrome://dino.
+                            You can play against your friends (or alone) across PC and Mobile and customize your dino with a nickname and skin!</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={null}>
+                    <img
+                        className="d-block w-100" src={exampleImg}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <button>Learn more</button>
+                        <p>Sample Text for Image Two</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={null}>
+                    <img
+                        className="d-block w-100" src={exampleImg}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <button>Learn more</button>
+                        <p>Sample Text for Image Three!!!</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
+        
+        {/* GALERIA DE PROJETOS /*/}
+        <div className='project-gallery'>
+            <h2>Projects I've worked on</h2>
+        </div>
+    </div>
     </>
     );
 }
