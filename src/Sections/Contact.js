6a0bdc9a8e757ffb2cddb99d9cbe5c1f71baa505
Marco Bossle Villanova <marco.bossle.villanova@gmail.com>
2025-09-28 21:45:03 -0300

    Updated color palette

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -21,10 +21,15 @@ function Contact()
                 <h1>Contact</h1>
                 <p>If you want to contact me, here are all the places you can find me!<br/>Feel free to send a message :)</p>
             </div>
-
+            
             <div className='contact-item' role="button" onClick={(e) => OpenTab('https://github.com/apecuca')}>
                 <img src={github_icon} alt='Social media logo'/>
                 <div className='contact-item-content'><p>https://github.com/apecuca</p></div>
+            </div>
+            
+            <div className='contact-item'>
+                <img src={email_icon} alt='Social media logo'/>
+                <div className='contact-item-content'><p>marco.bossle.villanova@gmail.com</p></div>
             </div>
 
             <div className='contact-item' role="button" onClick={(e) => OpenTab('https://apecuca.itch.io/')}>
@@ -32,19 +37,15 @@ function Contact()
                 <div className='contact-item-content'><p>https://apecuca.itch.io/</p></div>
             </div>
 
-            <div className='contact-item' role="button" onClick={(e) => OpenTab('https://www.instagram.com/pac_marco/')}>
-                <img src={instagram_icon} alt='Instagram logo'/>
-                <div className='contact-item-content'><p>@pac_marco</p></div>
-            </div>
-
             <div className='contact-item'>
                 <img src={phone_icon} alt='Phone logo'/>
                 <div className='contact-item-content'><p>+55 (48) 98835-2647</p></div>
             </div>
 
-            <div className='contact-item'>
-                <img src={email_icon} alt='Social media logo'/>
-                <div className='contact-item-content'><p>marco.bossle.villanova@gmail.com</p></div>
+
+            <div className='contact-item' role="button" onClick={(e) => OpenTab('https://www.instagram.com/pac_marco/')}>
+                <img src={instagram_icon} alt='Instagram logo'/>
+                <div className='contact-item-content'><p>@pac_marco</p></div>
             </div>
         </div>
     </>
