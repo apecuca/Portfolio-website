dfb10e03bbabaf329a1e4b75caf262e51a5f5054
Marco Bossle Villanova <marco.bossle.villanova@gmail.com>
2025-09-28 21:06:05 -0300

    Updated projects and about info

--- src/Sections/About.js
+++ src/Sections/About.js
@@ -12,6 +12,7 @@ import csharp_icon from "../Images/technologies/csharp_icon.png"
 import unity_icon from "../Images/technologies/unity_icon.png"
 import cpp_icon from "../Images/technologies/cpp_icon.png"
 import opengl_icon from "../Images/technologies/opengl_icon.png"
+import cmake_logo from '../Images/technologies/cmake_logo.png'
 
 function About()
 {
@@ -20,7 +21,7 @@ function About()
         <div id='AboutSection' className='about'> <div className='about-container'>
             <div className='about-infobox'>
                 <h1>Hi, I'm Marco!</h1>
-                <p>My name's Marco Bossle Villanova and I'm a game developer from Brazil. I have experience in the entire 
+                <p>My name's Marco Bossle Villanova and I'm a C++ developer from Brazil, currently working at Audaces. I have experience in the entire 
                     game development process, from planning to release and maintenance. My dream is to someday live abroad 
                     and work as a Game Engine developer.<br />
                     Additionally, I have some experience with full-stack web and app development.
@@ -29,6 +30,7 @@ function About()
                     <img src={csharp_icon} alt='C# language logo'/>
                     <img src={unity_icon} alt='Unity Engine logo'/>
                     <img src={cpp_icon} alt='C++ language logo'/>
+                    <img src={cmake_logo} alt='CMake logo'/>
                     <img src={opengl_icon} alt='OpenGL API logo'/>
                     <img src={html_icon} alt='HTML5 logo'/>
                     <img src={css_icon} alt='CSS3 logo'/>
--- src/Sections/Projects/Projects.js
+++ src/Sections/Projects/Projects.js
@@ -8,8 +8,10 @@ import { OpenTab } from '../Contact';
 import dinosBanner from "../../Images/banners/dinosBanner.png";
 import mussagameBanner from '../../Images/banners/mussagame_banner.jpg';
 import LUPEngineBanner from '../../Images/banners/LUPEngine_banner.jpg';
-import plagueBanner from '../../Images/banners/plagueprototype_banner.jpg';
+//import plagueBanner from '../../Images/banners/plagueprototype_banner.jpg';
 import portfolioBanner from '../../Images/banners/portfolio_banner.jpg';
+import patternBanner from '../../Images/banners/audPattern_banner.png';
+import miauwareBanner from '../../Images/banners/miauwareBanner.png';
 
 function FeaturedProjects()
 {
@@ -17,19 +19,6 @@ function FeaturedProjects()
     <div className='featured-projects'>
         <Carousel>
             {/* ITEM ONE */}
-            <Carousel.Item interval={15000}>
-                <img
-                    className="d-block w-100" src={dinosBanner}
-                    alt="Banner for the project called Dinos"
-                />
-                <Carousel.Caption> <div className='carousel-caption-content'>
-                    <button onClick={() => OpenTab('https://apecuca.itch.io/dinos')}>Learn More</button>
-                    <p>Dinos is a multiplayer clone of the famous offline browser game: chrome://dino.
-                        You can play against your friends (or alone) across PC and Mobile and customize your dino with a nickname and skin!</p>
-                </div> </Carousel.Caption>
-            </Carousel.Item>
-
-            {/* ITEM TWOO */}
             <Carousel.Item interval={15000}>
                 <img
                     className="d-block w-100" src={LUPEngineBanner}
@@ -42,15 +31,29 @@ function FeaturedProjects()
                 </div> </Carousel.Caption>
             </Carousel.Item>
 
-            {/* ITEM THREE */}
+            {/* ITEM TWO */}
             <Carousel.Item interval={15000}>
                 <img
-                    className="d-block w-100" src={portfolioBanner}
+                    className="d-block w-100" src={patternBanner}
                     alt="Banner for this website"
                 />
                 <Carousel.Caption> <div className='carousel-caption-content'>
-                    <button onClick={() => OpenTab('https://github.com/apecuca/Portfolio-website')}>Learn More</button>
-                    <p>This website! A fully responsive (I hope) static website, developed by me using the React.js framework.</p>
+                    <button onClick={() => OpenTab('https://audaces.com/en/solutions/pattern')}>Learn More</button>
+                    <p>Audaces Pattern is a powerful, computer-based software for creating, editing, and grading digital patterns in the fashion industry.
+                    I've worked on this project as a C++ developer.</p>
+                </div> </Carousel.Caption>
+            </Carousel.Item>
+
+            {/* ITEM THREE */}
+            <Carousel.Item interval={15000}>
+                <img
+                    className="d-block w-100" src={dinosBanner}
+                    alt="Banner for the project called Dinos"
+                />
+                <Carousel.Caption> <div className='carousel-caption-content'>
+                    <button onClick={() => OpenTab('https://apecuca.itch.io/dinos')}>Learn More</button>
+                    <p>Dinos is a multiplayer clone of the famous offline browser game: chrome://dino.
+                        You can play against your friends (or alone) across PC and Mobile and customize your dino with a nickname and skin!</p>
                 </div> </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
@@ -71,7 +74,11 @@ function GalleryItem(props)
             <div className='gallery-item-content'>
                 <p className='gallery-item-content-title'>{props.title}</p>
                 <p className='gallery-item-content-description'>{props.description}</p>
-                <button onClick={(e) => OpenTab(props.externalLink)}>Learn More</button>
+                {(props.externalLink === '') ? 
+                    <button>Not available</button> 
+                    : 
+                    <button onClick={(e) => OpenTab(props.externalLink)}>Learn More</button>
+                }
             </div>
         </div>
     );
@@ -86,26 +93,41 @@ function Projects()
             <h2>Projects I've worked on</h2>
             <div className='gallery-container'>
                 <GalleryItem
-                    banner={dinosBanner} title='Dinos' 
-                    description='Dinos is a multiplayer clone of the famous offline browser game: chrome://dino. This was a solo project.'
-                    externalLink='https://apecuca.itch.io/dinos'
-                />
-                <GalleryItem
-                    banner={mussagameBanner} title='Mussa Game V4'
-                    description="I've worked on Mussa Game V4 as an additional programmer, implementing small game mechanics and bugfixes."
-                    externalLink='https://play.google.com/store/apps/details?id=com.MaqnaInteractive.MussoumanoGame&hl=en'
-                />
-                <GalleryItem
                     banner={LUPEngineBanner} title='LUPEngine v1'
                     description="The first version of my own Game Engine, coded in C++ and OpenGL. 
                         This is an ongoing solo project with the sole purpose of studying game engine development."
                     externalLink='https://github.com/apecuca/LUPEngine_v1'
                 />
                 <GalleryItem
+                    banner={patternBanner} title='Audaces Pattern'
+                    description="Audaces Pattern is a powerful, computer-based software for creating, editing, and grading digital patterns in the fashion industry.
+                        I've worked on this project as a C++ developer."
+                    externalLink='https://audaces.com/en/solutions/pattern'
+                />
+                <GalleryItem
+                    banner={dinosBanner} title='Dinos' 
+                    description='Dinos is a multiplayer clone of the famous offline browser game: chrome://dino. This was a solo project.'
+                    externalLink='https://apecuca.itch.io/dinos'
+                />
+                <GalleryItem
+                    banner={miauwareBanner} title='Miauware'
+                    description="A game that combines the virtual pet and tower defense genres, where you take care of your antivirus cat while doing other things on the computer.
+                        This is an ongoing solo project, being developed as the final project for my bachelor's degree"
+                    externalLink=''
+                />
+                <GalleryItem
+                    banner={mussagameBanner} title='Mussa Game V4'
+                    description="A 3D horizontal runner of the YouTuber Mussoumano, developed and published by Maqna Interactive.
+                        I've worked on Mussa Game V4 as an additional programmer, implementing small game mechanics and bugfixes."
+                    externalLink='https://play.google.com/store/apps/details?id=com.MaqnaInteractive.MussoumanoGame&hl=en'
+                />
+                {/*
+                <GalleryItem
                     banner={plagueBanner} title='Plague Prototype'
                     description="Prototype for Plague, a fast roguelite platformer with a dark story. I've participated as the only programmer in this project."
                     externalLink='https://apecuca.itch.io/plague-prototype'
                 />
+                */}
                 <GalleryItem
                     banner={portfolioBanner} title='This website!'
                     description="A fully responsive (I hope) static website, developed by me using the React.js framework."
